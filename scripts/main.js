b91195f51e8221ba5eaf7143411b1db09312e613
alirafatartist <ltaft636@gmail.com>
2024-03-16 23:21:23 +0200

    Finsh Header & Home & Menu Sections & Js

---
+++ scripts/main.js
@@ -0,0 +1,43 @@
+const logo = document.querySelector(".logo");
+const menu = document.querySelector(".fa-bars");
+const links = document.querySelector("nav ul");
+// const img = document.querySelector("nav ul");
+
+function changeimgsrc() {
+  if (window.innerWidth <= 768) {
+    logo.setAttribute("src", "./assets/logo-white.png");
+  } else {
+    logo.setAttribute("src", "./assets/logo-dark.png");
+  }
+}
+changeimgsrc();
+
+window.addEventListener("resize", () => {
+  changeimgsrc();
+});
+
+// toggle menubar
+menu.addEventListener("click", () => {
+  menu.classList.toggle("fa-xmark");
+  menu.classList.toggle("rotate");
+  links.classList.toggle("show");
+});
+
+// scroll reveal
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+ScrollReveal().reveal(".home img", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+ScrollReveal().reveal(".home .content h2", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".home .content p", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
